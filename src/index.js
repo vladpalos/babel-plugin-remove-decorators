ec2e243256845e95a771e218179598f119a09870
Vlad Palos <vladpalos@gmail.com>
2015-09-25 10:35:11 +0100

    Removed enforced development NODE_ENV.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const env = process.env.NODE_ENV || 'development';
-
 export default function ({ Plugin, types: t }) {
   return new Plugin('remove-decorators', {
     visitor: {
       Decorator(node, parent) {
-        return (env == 'development') && this.dangerouslyRemove();
+        return this.dangerouslyRemove();
       }
     }
   });
